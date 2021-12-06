b016e481c59c1993ea5ae7d55a36caa7d2bbbef0
thucdx <thucdx31@gmail.com>
2021-12-06 14:28:16 +0700

    Day 4

--- day04/day04.py
+++ day04/day04.py
@@ -6,13 +6,15 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 drawn = []
 boards = []
 
-input_path = "sample"
+input_path = "input"
+
 
 def print_board(cur_board):
     print("#####")
     for row in cur_board:
         print(" ".join(map(str, row)))
     print("#####")
+
 
 with open("{0}/{1}".format(dir_path, input_path), "r") as f:
     drawn_line = f.readline()
@@ -36,7 +38,7 @@ with open("{0}/{1}".format(dir_path, input_path), "r") as f:
                 boards.append(cur_board)
             cur_board = []
         else:
-            numbers = list(map(int, re.split("\s+", line)))
+            numbers = list(map(int, re.split('\\s+', line)))
 
             cur_board.append(numbers)
 
@@ -45,40 +47,63 @@ with open("{0}/{1}".format(dir_path, input_path), "r") as f:
     for board in boards:
         print_board(board)
 
-def part_one():
+
+def first_board(boards):
     total_board = len(boards)
-    board_flag = [[[[False] * 5] * 5] * total_board]
+    board_flag = [[[False for _ in range(5)] for _ in range(5)] for _ in range(total_board)]
 
     for number in drawn:
+        # print("Check for {0}".format(number))
+
         for i in range(total_board):
             cur_board = boards[i]
+            cur_flag = board_flag[i]
+            # print_board(cur_flag)
+            # print("Shape {0} {1}".format(len(cur_flag), len(cur_flag[0])))
             bingo = False
             unmarked_sum = 0
+            # print_board(cur_flag)
             for j in range(5):
                 for k in range(5):
                     if cur_board[j][k] == number:
-                        board_flag[i][j][k] = True
+                        # print("Before assign ")
+                        # print_board(cur_flag)
+                        cur_flag[j][k] = True
+
                         # Check for bingo
-                        # all row
-                        if all(board_flag[i][j]):
-                            bingo = True
+                        bingo_by_row = all(cur_flag[j])
+                        bingo_by_col = all([cur_flag[t][k] for t in range(5)])
+                        # print_board(cur_flag)
+                        # print_board([[cur_flag[t][k] for t in range(5)]])
+                        bingo = bingo_by_row | bingo_by_col
+                        if bingo:
+                            print(i, j, k, bingo_by_row, bingo_by_col)
 
-                        # all column
-                        bingo_by_col = True
-                        for t in range(5):
-                            if not board_flag[i][t][k]:
-                                bingo_by_col = False
-                                break
-                        bingo |= bingo_by_col
-
-                    if not board_flag[i][j][k]:
+                    if not cur_flag[j][k]:
                         unmarked_sum += cur_board[j][k]
 
             if bingo:
-                print("Final score: ", unmarked_sum * number)
-                return
+                score = unmarked_sum * number
+                print("Final score: {0}, unmarked_sum: {1}, number: {2}"
+                      .format(unmarked_sum * number, unmarked_sum, number))
+                return i, score
 
     print("Not found")
+    return -1, 0
+
+
+def part_one():
+    first_board(boards)
+
+
+def part_two():
+    while len(boards) > 0:
+        index, score = first_board(boards)
+        boards.pop(index)
+
+        if len(boards) == 0:
+            print("Part two final score: {0}".format(score))
+
 
 if __name__ == "__main__":
-    part_one()
+    part_two()
